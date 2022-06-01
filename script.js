1190b6e131ca24c4198823feb05fe0eb470c4688
Sandra <sandra@imac-de-sandra.home>
2022-06-01 15:06:41 +0200

    Ajout fonctionnalité Menu mobile

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-/* Accordéon valeurs */
+// Menu mobile
 
+const menuButton = document.querySelector('#menuMobile');
+const menuBurger = document. querySelector('.menu__burger');
+const menuClose = document.querySelector('.menu__burger--close');
+const containerNav = document.querySelector('#navigationMobile');
+
+function toggleNavMobile(){
+    if(menuButton.getAttribute('aria-expanded') === 'true') {
+        menuButton.removeAttribute('aria-expanded');
+        menuBurger.style.display='';
+        menuClose.style.display='';
+        containerNav.style.display = '';
+    } else {
+        menuButton.setAttribute('aria-expanded', 'true');
+        menuBurger.style.display = 'none';
+        menuClose.style.display = 'block';
+        containerNav.style.display = 'flex';
+    }
+}
+
+menuButton.addEventListener('click', toggleNavMobile);
+
+
+/* Accordéon valeurs */
 
 const toggles = document.querySelectorAll('.metier__toggle');
 
